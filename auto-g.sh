0281dcb0ffa8c6e869af6275eee61db184fddfef
trilokinath7 <394951d++++++++++++++++++++++++++++++@gmail.com>
2024-06-23 21:46:38 +0530

    Update auto-g.sh

--- auto-g.sh
+++ auto-g.sh
@@ -32,7 +32,7 @@ echo "in - India (Mumbai)"
 ./ngrok tcp --region us 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=8g ashu930559/nomachine-ubuntu-desktop:g
+docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g ashu930559/nomachine-ubuntu-desktop:g
  
 clear
 echo "NoMachine: https://www.nomachine.com/download"
