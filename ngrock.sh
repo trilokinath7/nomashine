23a8fd5bc0c8d2d565e78846d47694755664ed06
trilokinath7 <394951d++++++++++++++++++++++++++++++@gmail.com>
2024-06-26 13:09:16 +0530

    Update ngrock.sh

--- ngrock.sh
+++ ngrock.sh
@@ -16,8 +16,7 @@ function goto
 : ngrok
 clear
 echo "Go to: https://dashboard.ngrok.com/get-started/your-authtoken"
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok config add-authtoken $CRP 
+./ngrok config add-authtoken 2iPLXyPCDkIwlR8gLB2nbynO6PN_3iYGWsKmaAL5UuqRrSDYN 
 clear
 echo "Repo: https://github.com/kmille36/Docker-Ubuntu-Desktop-NoMachine"
 echo "======================="
@@ -30,8 +29,7 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 4000 &>/dev/null &
+./ngrok tcp --region us 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=0.01g 
