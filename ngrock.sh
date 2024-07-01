16edd21c185522231d9f9ac65c259622e0baecd9
trilokinath7 <394951d++++++++++++++++++++++++++++++@gmail.com>
2024-07-01 08:55:43 +0530

    Update ngrock.sh

--- ngrock.sh
+++ ngrock.sh
@@ -41,6 +41,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: user
 echo Passwd: 123456
 echo "VM can't connect? Restart Cloud Shell then Re-run script."
-seq 1 20 | while read i; do echo -en "\r Running .     $i s /20 s";sleep 0.1;echo -en "\r Running ..    $i s /20 s";sleep 0.1;echo -en "\r Running ...   $i s /20 s";sleep 0.1;echo -en "\r Running ....  $i s /20 s";sleep 0.1;echo -en "\r Running ..... $i s /20 s";sleep 0.1;echo -en "\r Running     . $i s /20 s";sleep 0.1;echo -en "\r Running  .... $i s /20 s";sleep 0.1;echo -en "\r Running   ... $i s /20 s";sleep 0.1;echo -en "\r Running    .. $i s /20 s";sleep 0.1;echo -en "\r Running     . $i s /20 s";sleep 0.1; done
+seq 1 39960 | while read i; do echo -en "\r Running .     $i s /39960 s";sleep 0.1;echo -en "\r Running ..    $i s /39960 s";sleep 0.1;echo -en "\r Running ...   $i s /39960 s";sleep 0.1;echo -en "\r Running ....  $i s /39960 s";sleep 0.1;echo -en "\r Running ..... $i s /39960 s";sleep 0.1;echo -en "\r Running     . $i s /39960 s";sleep 0.1;echo -en "\r Running  .... $i s /39960 s";sleep 0.1;echo -en "\r Running   ... $i s /39960 s";sleep 0.1;echo -en "\r Running    .. $i s /39960 s";sleep 0.1;echo -en "\r Running     . $i s /39960 s";sleep 0.1; done
 gh codespace stop
 
