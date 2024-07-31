d95b0c873174ee661c236a8b663142b88bca3a60
trilokinath7 <394951d++++++++++++++++++++++++++++++@gmail.com>
2024-07-31 06:42:11 +0530

    Update auto-9.sh

--- auto-9.sh
+++ auto-9.sh
@@ -29,7 +29,7 @@ echo "au - Australia (Sydney)"
 echo "sa - South America (Sao Paulo)"
 echo "jp - Japan (Tokyo)"
 echo "in - India (Mumbai)"
-./ngrok http --region au 4000 &>/dev/null &
+./ngrok tcp --region au 4000 &>/dev/null &
 sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker run --rm -d --network host --privileged --name nomachine-xfce4 -e PASSWORD=123456 -e USER=user --cap-add=SYS_PTRACE --shm-size=1g ashu930559/nomachine-ubuntu-desktop:9
