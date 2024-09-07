d08136e31a1db68123e5555ef204614a323acc04
trilokinath7 <394951d++++++++++++++++++++++++++++++@gmail.com>
2024-09-07 17:14:19 +0530

    Update nomashine.sh

--- nomashine.sh
+++ nomashine.sh
@@ -70,9 +70,9 @@ sleep 1
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
 docker logs nomashine
 sleep 1
-docker logs nomashine
 clear
 clear
+
 curl ifconfig.me
 echo
 echo
