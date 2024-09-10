b3a4650cd9af854907d9216eef0ed31580fa6c3e
trilokinath7 <trilokinath+7@outlook.in>
2024-09-10 08:09:59 +0530

    Update nomashine.sh

--- nomashine.sh
+++ nomashine.sh
@@ -78,8 +78,9 @@ echo
 echo
 sleep 1
 
-public_url=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p' | tr -d :)
+public_url=$(curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*"public_url":"(tcp:\/\/[^"]*).*/\1/p')
 echo "Public URL: $public_url"
+
 
 echo
 echo
