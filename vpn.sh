fd8f3cc201a8f8fadeac2af3f22b2875eae0015e
trilokinath7 <trilokinath+7@outlook.in>
2024-09-28 14:38:21 +0530

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -27,5 +27,5 @@ fi
 # Update resolv.conf with a specific nameserver
 echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 
-# Start OpenVPN with the config file
-sudo openvpn --config "$CONFIG_PATH"
+# Start OpenVPN with the given config file
+sudo openvpn --config /etc/openvpn/vpngate.ovpn
