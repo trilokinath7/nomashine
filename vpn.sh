1f8f848303837ab9d92a9aef481edca53f49795f
trilokinath7 <trilokinath+7@outlook.in>
2024-09-28 13:23:37 +0530

    Update vpn.sh

--- vpn.sh
+++ vpn.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-# Install OpenVPN if not installed
-sudo apt update
-sudo apt install -y openvpn wget
+# Update resolv.conf with a specific nameserver
+echo "nameserver 8.8.8.8" | sudo tee /etc/resolv.conf > /dev/null
 
-#!/bin/bash
-
-# Prompt user for the OpenVPN URL
-read -p "Enter the OpenVPN download URL: " vpn_url
-
-# Specify the location where the OVPN file will be saved
-output_file=~/Desktop/vpngate.ovpn
-
-# Use wget to download the file from the provided URL
-wget -O "$output_file" "$vpn_url"
-
-# Check if the download was successful
-if [ $? -eq 0 ]; then
-    echo "OpenVPN configuration file downloaded successfully to $output_file."
-else
-    echo "Failed to download the OpenVPN configuration file. Please check the URL."
-fi
-
-sudo mkdir -p /etc/openvpn  # Create the directory if it doesn't exist
-sudo mv ~/Desktop/vpngate.ovpn /etc/openvpn/
-
-
+# Start OpenVPN with the given config file
 sudo openvpn --config /etc/openvpn/vpngate.ovpn
