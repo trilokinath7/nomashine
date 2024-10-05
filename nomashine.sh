e9c854b46d6ec885af6a7c4dd5450e804bd68894
trilokinath7 <trilokinath+7@outlook.in>
2024-10-05 19:37:03 +0530

    Update nomashine.sh

--- nomashine.sh
+++ nomashine.sh
@@ -118,18 +118,19 @@ done
 pkill ngrok
 clear
 
-seq 1 17000 | while read i; do 
-    echo -en "\r Running .     $i s /17000 s"; sleep 0.1
-    echo -en "\r Running ..    $i s /17000 s"; sleep 0.1
-    echo -en "\r Running ...   $i s /17000 s"; sleep 0.1
-    echo -en "\r Running ....  $i s /17000 s"; sleep 0.1
-    echo -en "\r Running ..... $i s /17000 s"; sleep 0.1
-    echo -en "\r Running     . $i s /17000 s"; sleep 0.1
-    echo -en "\r Running  .... $i s /17000 s"; sleep 0.1
-    echo -en "\r Running   ... $i s /17000 s"; sleep 0.1
-    echo -en "\r Running    .. $i s /17000 s"; sleep 0.1
-    echo -en "\r Running     . $i s /17000 s"; sleep 0.1
+seq 1 18000 | while read i; do 
+    echo -en "\r Running .     $i s /18000 s"; sleep 0.1
+    echo -en "\r Running ..    $i s /18000 s"; sleep 0.1
+    echo -en "\r Running ...   $i s /18000 s"; sleep 0.1
+    echo -en "\r Running ....  $i s /18000 s"; sleep 0.1
+    echo -en "\r Running ..... $i s /18000 s"; sleep 0.1
+    echo -en "\r Running     . $i s /18000 s"; sleep 0.1
+    echo -en "\r Running  .... $i s /18000 s"; sleep 0.1
+    echo -en "\r Running   ... $i s /18000 s"; sleep 0.1
+    echo -en "\r Running    .. $i s /18000 s"; sleep 0.1
+    echo -en "\r Running     . $i s /18000 s"; sleep 0.1
 done
+
 clear
 
 
