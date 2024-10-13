d95118d678842ad3de8e28ba3da8e827ec3b07ee
trilokinath7 <trilokinath+7@outlook.in>
2024-10-13 21:01:34 +0530

    Update nowindows.sh

--- nowindows.sh
+++ nowindows.sh
@@ -68,7 +68,9 @@ fi
 clear
 sleep 2
 if curl --silent --show-error http://127.0.0.1:4040/api/tunnels  > /dev/null 2>&1; then echo OK; else echo "Ngrok Error! Please try again!" && sleep 1 && goto ngrok; fi
-sleep 30
+sleep 2
+docker logs nomashine
+docker logs nomashine
 clear
 clear
 clear
